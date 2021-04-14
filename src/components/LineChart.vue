41112b334219317263ea116c06df8e58a682cde6
Stas N <stas-nepomn@ya.ru>
2021-04-14 14:56:34 +0800

    Dashboard view

---
+++ src/components/LineChart.vue
@@ -0,0 +1,21 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,41 @@
 <template lang="pug">
-section Dashboard
+section
+  v-row
+    v-col(v-for="n in 3", :key="n", cols="6")
+      v-card(outlined)
+        v-card-title Funnel
+        v-card-subtitle Funnel description
+        v-card-text(style="text-align: center") 
+          LineChart(:chartdata="chartdata", :options="chartOptions")
+        v-card-actions
+          v-spacer
+          v-btn(text) Export data
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import LineChart from "@/components/LineChart.vue";
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: {
+    LineChart
+  }
+})
+export default class Home extends Vue {
+  chartdata = {
+    labels: ["Login", "AddTask", "DeleteTask"],
+    datasets: [
+      {
+        data: [40, 10, 5]
+      }
+    ]
+  };
+  chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    }
+  };
+}
 </script>
